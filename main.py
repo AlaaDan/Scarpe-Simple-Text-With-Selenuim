a9ce2545f75bf671e24c4bacb493c569bcc3c671
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2022-05-04 14:03:46 +0000

    Login and click script

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
+import time
 def get_driver():# Set options to make browsing easier for us. 
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobar")
@@ -8,13 +9,19 @@ def get_driver():# Set options to make browsing easier for us.
   options.add_argument("no-sandbox")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink=features=AutomationControlled")
+  
   driver = webdriver.Chrome(options=options)
-  driver.get("https://automated.pythonanywhere.com/")
+  driver.get("https://automated.pythonanywhere.com/login/")
   return driver 
   
 def main():
   driver = get_driver()
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-  return element.text
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  print(driver.current_url)
+  
 
 print(main())
